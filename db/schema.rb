e3c5a112314ecd47749c752e0a634313fe9c0f5e
AddieRose <addierose444@gmail.com>
2021-10-30 00:10:31 -0400

    Changes commited to pull in new changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_10_29_152655) do
-
+ActiveRecord::Schema.define(version: 2021_08_30_180952) do
 
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.timestamp "CheckoutTime"
-    t.timestamp "CheckinTime"
   end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.string "name"
     t.string "address"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "Capacity"
-    t.string "Description"
-    t.string "LandmarksList"
-  end
-
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "Email"
-    t.string "Username"
-    t.string "Password"
-    t.integer "CreditCardNum"
-    t.integer "ExpDate"
-    t.integer "SecurityCode"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
