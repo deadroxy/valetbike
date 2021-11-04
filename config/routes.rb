943204623d7c5d8b30a594a4ba4e4d9673f3f16a
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2021-11-04 16:29:31 -0400

    create welcome page and all other files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
-  get 'login' to: 'sessions#new'
+  get 'login', to: 'sessions#new'
 
-  post 'login' to: 'sessions#create'
+  post 'login', to: 'sessions#create'
 
   get 'welcome', to: 'sessions#welcome'
   
---
+++ db/migrate/20211104194320_create_uesrnames.rb
@@ -0,0 +1,9 @@
+class CreateUesrnames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :uesrnames do |t|
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
