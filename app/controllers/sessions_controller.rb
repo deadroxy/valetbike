6a8101d154181a35af204b05c3e0e5f29aac5251
rzvulpe <amorecut@smith.edu>
2021-11-09 15:42:57 -0500

    login page + authorization schematic where there is private info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,29 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
   def new
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+
+        # line 14 (right under this comment) was in the original article but giving me an error message
+
+        # sessions[:user_id] = @user.id
+
+        # the error was that i hadn't created/defined a local variable/method named sessions, which is true, but it also
+        # offered the suggestion to change it to 'session' which i did (see below), but i don't know if
+        # this messes up anything ;-;
+
+        session[:user_id] = @user.id
+
+        redirect_to '/welcome'
+    else
+        redirect_to '/login'
+    end
+
   end
 
   def login
@@ -10,4 +31,9 @@ class SessionsController < ApplicationController
 
   def welcome
   end
+
+  def page_requires_login
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  
 end
