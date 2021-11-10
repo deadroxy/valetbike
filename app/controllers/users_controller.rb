fc3e8c270535b3fc1062412a07226193ffc3fbe1
AddieRose <addierose444@gmail.com>
2021-11-09 23:49:09 -0500

    Added basic footer, restricted height of body to prevent scrolling for lightly populated pages. Fix routing after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      render "/login/index"
+      render "/home/index" # Route user to homepage on account creation
     else
       flash.now[:alert] ||= ""
       @user.errors.full_messages.each do |message|
