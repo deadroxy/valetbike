32d1326e20c0d28f0b1ca47fdf167eb6b5d98b7d
Sethatevy Bong <sethatevybong@Tevs-MacBook-Pro.local>
2021-11-12 23:01:18 -0500

    Tevy_ update my database.yml with password for the dev. env.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_031600) do
+ActiveRecord::Schema.define(version: 2021_08_30_180952) do
 
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 2021_09_21_031600) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "docked_bike_count"
-    t.string "integer"
   end
 
 end
