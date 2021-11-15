4ec89c4d944c962b9d036ea487f0469ea0d7383c
brend-ag <bgutierrez@smith.edu>
2021-11-15 18:34:39 -0500

    Attempted to link About Tab in Landing Page

---
+++ app/controllers/tabs_controller.rb
@@ -0,0 +1,8 @@
+# Purpose: This controller handles the about and process pages
+class TabsController < ApplicationController
+
+    def show
+    #  render template: "views/tab_pages/#{params[:page]}"
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: "static#landing"
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :tabs, only: [:show]
+  get '/views/tab_pages/', to: 'tab_pages#index', as: :tab_pages
+#  get "/views/tab_pages/:page" => "tab_pages#{show}"
+
 end
