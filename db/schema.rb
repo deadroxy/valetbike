1bd45a601bf42b1cb48daeeb2b37fe31db29ee30
AddieRose <addierose444@gmail.com>
2021-11-16 23:15:38 -0500

    Positioned station pins from database info. Created station pins dynamically with templates. Postioning is now relative to the mmap image, or more acurratly the parent map div. Rake task not updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_195233) do
+ActiveRecord::Schema.define(version: 2021_11_17_035439) do
 
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "checkoutTime"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_11_06_195233) do
     t.text "landmarksList"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "topLocation"
+    t.float "leftLocation"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
