3cd16a3d3d8f8bfe99c73dcc7df01291bc2d9cb0
Sophia Hager <sophiahager@Sophias-MacBook-Air.local>
2021-11-16 21:57:28 -0500

    basic stations page, will update to have relevant information and look good

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,9 @@ class MapController < ApplicationController
   def index
       @stations = Station.all.order(id: :asc)
   end
+
+  def show
+    @station = Station.find_by_id(params[:id])
+  end
   
 end
