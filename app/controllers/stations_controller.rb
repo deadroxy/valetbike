69ca96dc252bd9966ac793405ef1456597f80348
Winnie0v0 <wzong@smith.edu>
2021-11-23 16:31:13 -0500

    map updated!

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,5 +1,11 @@
 class StationsController < ApplicationController
-  
+  before_action :allow_iframe_requests, only: [:index]
+
+  def allow_iframe_requests
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://maps.googleapis.com'
+    # response.headers.delete "X-Frame-Options"
+  end
+
   def index
     @stations = Station.all.order(identifier: :asc)
   end
