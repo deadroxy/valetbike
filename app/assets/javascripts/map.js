edcb3da9a1bc869f0d365fae865d7f18b7bff817
sethateybong <sethatevy98@gmail.com>
2021-11-26 13:09:03 -0500

    Configure marker menu

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,27 +2,55 @@
 // focusable markers.
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
+      zoom: 14,
       center: { lat: 42.31710100023048, lng: -72.64019893342778 },
     });
     // Set LatLng and title text for the markers. The first marker (Boynton Pass)
     // receives the initial focus when tab is pressed. Use arrow keys to
     // move between markers; press tab again to cycle through the map controls.
+
   const contentString1 =
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Music Academy Station</h1>' +
+
+    '<h1 id="firstHeading" class="firstHeading">Village Hill/State Hospital</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Music Academy</b> <p>" +
-    '<p>want to go there? <a href="https://www.google.com/maps/place/42%C2%B019\'04.4%22N+72%C2%B038\'00.3%22W/@42.3184578,-72.6348208,21z/data=!4m5!3m4!1s0x0:0xb7172666928fc1f4!8m2!3d42.3178889!4d-72.6334167">' +
-    "navigate</a> </p>" +
+    "<p><b>51 Village Hill Rd #3600, Northampton, MA 01060</b> <p>" +
+    '<p>Want to bike there? <a href="https://www.google.com/maps/dir/42.3129308,-72.6270902/Villages+At+Hospital+Hill,+51+Village+Hill+Rd+%233600,+Northampton,+MA+01060/@42.3129925,-72.654628,14z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89e6d77c78f9c3d1:0x8adc59cea4260f0!2m2!1d-72.646384!2d42.312471!3e1">' +
+    "Directions</a> </p>" +
     "</div>" +
     "</div>";
+
+const contentString2 =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+
+ '<h1 id="firstHeading" class="firstHeading">Florence Bank Station</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>19 Meadow Street Florence MA @ Lilly Library</b> <p>" +
+    '<p>Want to bike there? <a href="https://www.google.com/maps/dir/42.3129308,-72.6270902/Villages+At+Hospital+Hill,+51+Village+Hill+Rd+%233600,+Northampton,+MA+01060/@42.3129925,-72.654628,14z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89e6d77c78f9c3d1:0x8adc59cea4260f0!2m2!1d-72.646384!2d42.312471!3e1">' +
+    "Directions</a> </p>" +
+    "</div>" 
+
+ '<h1 id="firstHeading" class="firstHeading">Village Hill/State Hospital</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>51 Village Hill Rd #3600, Northampton, MA 01060</b> <p>" +
+    '<p>Want to bike there? <a href="https://www.google.com/maps/dir//19+Meadow+Street,+Florence,+Northampton,+Massachusetts/@42.3246157,-72.685409,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x89e6d7a4b49620fb:0x61153f756bd59603!2m2!1d-72.6741531!2d42.3355895!3e1">' +
+    "Directions</a> </p>" +
+    "</div>" 
+
++
+    "</div>";
+
+
+
   const tourStops = [
-    [{ lat: 42.31790004551642, lng: -72.63340923034824 }, contentString1],
-      [{ lat: 42.31614494130486, lng: -72.63545914647698 }, "ValleyBike Share"],
-      [{ lat: 42.32029329055504, lng: -72.63977475937963 }, "ValleyBike Share"],
+    [{ lat: 42.31263094188917, lng:-72.64626991303041}, contentString1],
+    [{ lat: 42.3355982502079, lng:-72.67411731294217}, contentString21],
+    [{ lat: 42.31614494130486, lng: -72.63545914647698 }, "ValleyBike Share"]
+   
     ];
     // Create an info window to share between markers.
     const infoWindow = new google.maps.InfoWindow();
