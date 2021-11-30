a79de7ca64f4c5b2ac3a3742efecbc15ecff1a59
wninohira <wninohira@smith.edu>
2021-11-30 15:40:32 -0500

    connect user and bike when user check out a bike

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_060159) do
+ActiveRecord::Schema.define(version: 2021_11_30_201449) do
 
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_060159) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "current_bike_id"
   end
 
 end
