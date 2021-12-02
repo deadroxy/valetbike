be0c2196abca27f9031828778e52e928589f3da8
carolynnguyen614 <carolynnguyen614@gmail.com>
2021-12-02 10:22:56 -0500

    working on user to payment page issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "stations#index"
   resources :users #,  only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  get 'payment/index'
+  get 'authorized', to: 'sessions#payments'
 end
