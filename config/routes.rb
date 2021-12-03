5cf26be405eb6ab02e6a8ccd478609800d127c62
rzvulpe <amorecut@smith.edu>
2021-12-03 16:54:09 -0500

    payment page reworked

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,7 @@ Rails.application.routes.draw do
   get 'payment', to: 'sessions#payment'
   get 'checkout', to: 'sessions#checkout'
   post 'checkout', to: 'sessions#check'
-<<<<<<< HEAD
-  get 'payment', to: 'sessions#payment'
-=======
   post 'logout', to: 'sessions#logout'
->>>>>>> 408e7f0973c42ced99fbf0cd2af80537ca85b2fe
 
 
   resources :places
