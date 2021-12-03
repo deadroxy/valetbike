20b698c1f3f5e167b7ce9d6cf8e7b8295db6b6e6
wninohira <wninohira@smith.edu>
2021-12-03 15:26:51 -0500

    modified mifration file to adjust data...

--- db/migrate/20211201012517_add_user_info.rb
+++ db/migrate/20211201012517_add_user_info.rb
@@ -2,6 +2,6 @@ class AddUserInfo < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-  
+    add_column :users, :email, :string
   end
 end
