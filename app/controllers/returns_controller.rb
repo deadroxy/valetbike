627617bb7e169690436e778db994264222a62d59
jennyzyt <yzhang18@smith.edu>
2021-12-06 00:43:25 -0500

    unfinished: add returned bike to new station, update both bike.current_station_id adn station.docked_bikes

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,6 +1,21 @@
 class ReturnsController < ApplicationController
-    def index
-  
+  def index
+    @stations = Station.all
+  end
+  def update
+    @station = Station.find_by(identifier: params[:stationId])
+    @bike = Bike.find_by(identifier: params[:bikeId])
+    @bike.current_station_id = @station
+    @station.docked_bikes.append(@bike)
+    respond_to do |format|
+      if @bike.update_attribute(:current_station, nil)
+        flash[:notice] = "Please confirm return below:"
+        format.html { redirect_to({:controller => 'return', :action => 'index' })}
+      else
+        flash[:notice] = "Return was unsuccessful. Please try another station."
+        format.html { redirect_to({:controller => 'return', :action => 'index' })}
+      end
     end
   end
+end
   
