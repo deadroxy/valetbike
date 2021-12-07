0ab714c70a2edd49d68c23faab88e1c1ae30ea51
rzvulpe <amorecut@smith.edu>
2021-12-06 19:45:00 -0500

    Made commentary on files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # Check user's input of username and password
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
 
         redirect_to '/authorized'
     else
-        flash[:error] = "Incorrect username or password"
+        flash[:error] = "Incorrect username or password" # Send error when redirected
         redirect_to '/login'
     end
 
@@ -30,6 +31,7 @@ class SessionsController < ApplicationController
   def about
   end
 
+  # This is for map page
   def page_requires_login
     @stations = Station.all.order(identifier: :asc)
     @bikes = Bike.all.order(identifier: :asc)
@@ -38,25 +40,31 @@ class SessionsController < ApplicationController
   def payment
   end
 
+  # User can type in the bike code to rent the bike
+  # This page is aware of what station is specified
   def checkout
+    # Check if the query of station_identifier exists in url
     if params[:station_identifier]
       session[:station_identifier] = params[:station_identifier].to_i
     end
     @station = Station.find_by_identifier(session[:station_identifier])
   end
 
+  # Process check-out request
   def check
+    # if user has a bike checked out already, they get redirected to check-in page
     if current_user.current_bike_id
       flash[:alart] = "You cannot check out more than one bike. Please return your bike to a station"
       redirect_to '/checkin' and return
     end
     b = Bike.find_by_identifier(params[:bikeid])
+    # Check if the bike user is trying to check out exists at the station specified
     if b && session[:station_identifier] == b.current_station_identifier
       current_user.update_attribute(:current_bike_id, params[:bikeid])
       b.update_attribute(:current_station_identifier, nil)
       redirect_to '/ride'
     else
-
+      # if the bike doesn't exist in the specified station, send error
       flash[:error] = "Could not find the bike at this station"
       redirect_to '/checkout'
     end
@@ -71,6 +79,7 @@ class SessionsController < ApplicationController
   def about
   end
 
+  # Process logout request
   def logout
     session[:user_id]= nil
     redirect_to '/welcome'
@@ -86,6 +95,7 @@ class SessionsController < ApplicationController
     @stations = Station.all.order(identifier: :asc)
   end
 
+  #Processes bike check in
   def process_checkin
     @bike = Bike.find_by_identifier(current_user.current_bike_id)
     @bike.update_attribute(:current_station_identifier, params[:station_identifier])
