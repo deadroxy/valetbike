4204921c0fdfe483912727e2edf3165222520506
elainezhang <ezzhang@smith.edu>
2022-01-29 20:18:24 -0500

    assignment 1 - reverse order of stations, welcome msg

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,5 +3,8 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
   end
-  
+
+  def reverse 
+    @stations = Station.all.order(identifier: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "stations#index"
+
+  get "/stations/:id", to: "stations#reverse"
 end
