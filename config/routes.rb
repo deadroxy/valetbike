ba4e8c2dd8586f4b1972fbfba169ddb026d2d011
Dorothy Addie <daddie@smith.edu>
2022-03-18 20:42:12 -0400

    register page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "login", to: "login#index"
   get "membership", to: "membership#index"
   get "map", to: "map#index"
+  get "register", to: "register#index"
 end
