a17bcca4bd35e93fb1460038a41d4c332411597e
Casey Mae Perez <52713498+CaseyMaePerez@users.noreply.github.com>
2022-04-14 18:20:07 -0400

    Create remaining pages in pricing controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "home#index"
-  get 'home/howitworks'
-  get 'home/map'
-  get 'home/pricing'
-  get 'home/about'
-  get 'home/help'
-  get 'home/privacy'
-  get 'home/terms_of_use'
-  get 'home/my_account'
-  get 'home/rent'
-  get 'pricing/pay_per_ride'
+  get 'how_it_works', to: 'home#how_it_works'
+  get 'map', to: 'home#map'
+  get 'pricing', to: 'home#pricing'
+  get 'about', to: 'home#about'
+  get 'help', to: 'home#help'
+  get 'privacy', to: 'home#privacy'
+  get 'terms_of_use', to: 'home#terms_of_use'
+  get 'my_account', to: 'home#my_account'
+  get 'rent', to: 'home#rent'
+  get 'pricing/pay_per_ride_pass'
+  get 'pricing/day_pass'
+  get 'pricing/monthly_membership'
+  get 'pricing/annual_membership'
+  get 'pricing/student_annual_membership'
+  get 'pricing/access_pass'
+  get 'pricing/founding_member'
 end
