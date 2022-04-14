fbf0e0c03e748efa03fe0893614b462e29d5606e
Casey Mae Perez <52713498+CaseyMaePerez@users.noreply.github.com>
2022-04-14 17:56:08 -0400

    Reconfigured home controller links for UX

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  get 'home/howitworks'
-  get 'home/map'
-  get 'home/pricing'
-  get 'home/about'
-  get 'home/help'
-  get 'home/privacy'
-  get 'home/terms_of_use'
-  get 'home/my_account'
-  get 'home/rent'
+  get 'how_it_works', to: 'home#how_it_works'
+  get 'map', to: 'home#map'
+  get 'pricing', to: 'home#pricing'
+  get 'about', to: 'home#about'
+  get 'help', to: 'home#help'
+  get 'privacy', to: 'home#privacy'
+  get 'terms_of_use', to: 'home#terms_of_use'
+  get 'my_account', to: 'home#my_account'
+  get 'rent', to: 'home#rent'
 end
