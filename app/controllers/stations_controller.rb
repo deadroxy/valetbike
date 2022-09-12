6e68b916440f058a4b94e5aeff6a09353ba58fd8
bobiamack <barcemack@smith.edu>
2022-09-12 01:26:32 -0400

    created status (of stations) page) in controller, views, and routes.rb

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,7 +1,23 @@
+require 'csv'
+
 class StationsController < ApplicationController
   
   def index
     @stations = Station.all.order(identifier: :asc)
+    # read csv here
+    # for each row in station-data.csv (column), create an instance of Station (Station.new)
+    
+    # file = File.basename("~/Development/valetbike/notes/station-data.csv")
+    # csv = CSV.parse(File.read(file), headers: true)
+
+    # csv.each do |row|
+    #   Station.create!(row.to_hash)
+    # end
+  end
+
+  def status
+    # hard code station-data.csv
+    # https://stackoverflow.com/questions/4410794/ruby-on-rails-import-data-from-a-csv-file
   end
   
 end
