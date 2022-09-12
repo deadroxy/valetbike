fb9548d8e1b2e9409d7960c48b07c1c695bfed3a
bobiamack <barcemack@smith.edu>
2022-09-12 01:56:56 -0400

    add bikes method

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -18,6 +18,11 @@ class StationsController < ApplicationController
   def status
     # hard code station-data.csv
     # https://stackoverflow.com/questions/4410794/ruby-on-rails-import-data-from-a-csv-file
+    #TODO: figure out how to use csv file from valetbike/notes/station-data.csv
   end
   
+  def bikes
+    #TODO: create bike list and bike page (bikes.html.erv)
+  end
+
 end
