c711e08222f516c020c5cc1853e597c7fa9cfcc1
sylvia <stan@smith.edu>
2022-09-12 22:13:29 -0400

    Initial commit.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,5 +3,5 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
   end
-  
+
 end
--- db/migrate/20220831090719_create_bikes.rb
+++ db/migrate/20220831090719_create_bikes.rb
@@ -1,10 +1,10 @@
 class CreateBikes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :bikes do |t|
-      t.integer :identifier
-      t.integer :current_station_id
-
-      t.timestamps
+    def change
+      create_table :bikes do |t|
+        t.integer :identifier
+        t.integer :current_station_id
+  
+        t.timestamps
+      end
     end
   end
-end
