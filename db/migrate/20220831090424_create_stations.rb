126cf1e1c37a576043c4790e161fb28fc07c882d
Evelyn Gao <ygao@smith.edu>
2022-09-18 23:04:55 -0400

    add features

--- db/migrate/20220831090424_create_stations.rb
+++ db/migrate/20220831090424_create_stations.rb
@@ -4,6 +4,7 @@ class CreateStations < ActiveRecord::Migration[7.0]
       t.integer :identifier
       t.string :name
       t.string :address
+      t.integer :docked_bike_count
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_090719) do
     t.integer "identifier"
     t.string "name"
     t.string "address"
+    t.integer "docked_bike_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
