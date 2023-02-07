b09a16e62d787844d35f7ae69a91c06648bf59b7
cyberchai <chairachananharder@gmail.com>
2023-02-06 20:23:07 -0500

    Number of docked bikes visible on the page & other

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,10 @@
 class StationsController < ApplicationController
   
   def index
+    @stations = Station.all.order(identifier: :asc)
+  end
+  
+  def reverse
     @stations = Station.all.order(identifier: :desc)
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "stations#index"
+  
+#  get "/stations/:identification", to: "stations#index"
+    get "/stations/:identifier", to: "station#reverse", as: "station_rev"
 end
