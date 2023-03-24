067389f4d774e7f2f9e3dbbadd1ff96f99de11ee
PurplePanda95 <epneuwalder@gmail.com>
2023-03-24 16:57:22 -0400

    Wrote tests for payment class (this also modified the schema.rb file) required date class in test_helper to make it easier to run tests. Rewrote the custom validations for payment class based on test feedback

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -10,7 +10,7 @@ class Payment < ApplicationRecord
     validate :has_rental_or_membership
     validate :purpose_matches_association
     validates_numericality_of :amount, greater_than_or_equal_to: 0
-
+    
     # belongs_to :card, class_name: :Card, foreign_key: :card_id
     # belongs_to :user, class_name: :User, foreign_key: :user_id
     # belongs_to :rental, class_name: :Rental, foreign_key: rental_id, optional: true
@@ -26,9 +26,25 @@ class Payment < ApplicationRecord
     private
     # returns true if this payment belongs to either a membership or a rental but not both
     def has_rental_or_membership
-        (rental_id.present? || membership_id.present?) && !(rental_id.present? && membership_id.present?)
+        unless rental_id.present? || membership_id.present?
+            errors.add(:rental_id, "must be present if membership id is not")
+            errors.add(:membership_id, "must be present if rental id is not")
+        end
+        if rental_id.present? && membership_id.present?
+            errors.add(:rental_id, "cannot be present if payment has membership id")
+            errors.add(:membership_id, "cannot be present if payment has rental id")
+        end
     end
     def purpose_matches_association
-        (rental_id.present? && purpose == "rental") || (membership_id.present? && purpose == "membership")
+        if rental_id.present?
+            unless purpose == "rental"
+                errors.add(:purpose, "must be rental if rental_id is present")
+            end
+        end 
+        if membership_id.present?
+            unless purpose == "membership"
+                errors.add(:purpose, "must be membership if membership_id is present")
+            end
+        end
     end
 end
--- test/models/payment_test.rb
+++ test/models/payment_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class PaymentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Payment gets created" do
+    date = DateTime.new(2001,2,3,4,5,6)
+    payment = Payment.new
+    payment.identifier = 0
+    payment.card_id = 6
+    payment.rental_id = 4
+    payment.user_id = 3
+    payment.purpose = "rental"
+    payment.amount = 1
+    payment.date = date
+
+    assert payment.save
+  end
+  test "Payment can't have rental and membership" do
+    date = DateTime.new(2001,2,3,4,5,6)
+    payment = Payment.new
+    payment.identifier = 0
+    payment.card_id = 6
+    payment.rental_id = 4
+    payment.membership_id = 2
+    payment.user_id = 3
+    payment.purpose = "rental"
+    payment.amount = 1
+    payment.date = date
+
+    assert_not payment.save
+  end
 end
