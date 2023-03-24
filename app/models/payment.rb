945316104c6287972064c2c1ff8e93943a72eb5b
PurplePanda95 <epneuwalder@gmail.com>
2023-03-24 16:10:18 -0400

    Added validations to payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,6 +6,11 @@ class Payment < ApplicationRecord
                             :card_id,
                             :date
     validates_uniqueness_of :identifier
+    validates_inclusion_of :purpose, in: Array["rental", "membership"]
+    validate :has_rental_or_membership
+    validate :purpose_matches_association
+    validates_numericality_of :amount, greater_than_or_equal_to: 0
+
     # belongs_to :card, class_name: :Card, foreign_key: :card_id
     # belongs_to :user, class_name: :User, foreign_key: :user_id
     # belongs_to :rental, class_name: :Rental, foreign_key: rental_id, optional: true
@@ -18,4 +23,12 @@ class Payment < ApplicationRecord
     # def getUser
     #     @purpose
     # end
+    private
+    # returns true if this payment belongs to either a membership or a rental but not both
+    def has_rental_or_membership
+        (rental_id.present? || membership_id.present?) && !(rental_id.present? && membership_id.present?)
+    end
+    def purpose_matches_association
+        (rental_id.present? && purpose == "rental") || (membership_id.present? && purpose == "membership")
+    end
 end
