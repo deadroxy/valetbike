e757840b6deeb4726bdbaacc7f07553b86dca575
DESKTOP-1VAQE60\Y <ychen48@smith.edu>
2023-03-25 15:40:22 -0400

    Updated Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,11 +1,33 @@
 class Station < ApplicationRecord
-  validates_presence_of    :identifier,
-                           :name,
-                           :address
-  validates_uniqueness_of  :identifier
+  validates_presence_of    :station_id,
+                           :station_name,
+                           :address,
+                           :has kiosk,
+                           :num_docks
+  validates_uniqueness_of  :station_id
   
   has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
   def getAddress 
     @address
+  end
+  
+  def has_bikes
+    if @bikes >= 1
+        true
+    end
+  end
+
+  def get_num_docks
+    @num_docks
+  end
+
+  def get_num_bikes
+    #return number of bikes docked at Station
+    @bikes.count
+  end
+
+  def get_available_docks
+    #return number of empty docks at station
+    @num_docks - @bikes.count
   end
 end
