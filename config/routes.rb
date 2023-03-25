436cfc65a494c1fd2fe984382d91408cace4a6dd
PurplePanda95 <epneuwalder@gmail.com>
2023-03-25 17:21:08 -0400

    Deleted unnecessary views

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   get 'membership/index'
   get 'membership/show'
   get 'membership/new'
-  get 'membership/create'
   get 'membership/edit'
-  get 'membership/update'
   get 'membership/delete'
-  get 'membership/destroy'
   root to: "stations#index"
 end
