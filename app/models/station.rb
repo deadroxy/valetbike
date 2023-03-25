04a892acd16050c6f8e51b3f9b591af4f93a6a05
DESKTOP-1VAQE60\Y <ychen48@smith.edu>
2023-03-25 16:19:38 -0400

    Updated methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -8,26 +8,26 @@ class Station < ApplicationRecord
   
   has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
   def getAddress 
-    @address
+    address
   end
   
   def has_bikes
-    if @bikes >= 1
+    if docked_bikes >= 1
         true
     end
   end
 
   def get_num_docks
-    @num_docks
+    num_docks
   end
 
   def get_num_bikes
     #return number of bikes docked at Station
-    @bikes.count
+    docked_bikes.count
   end
 
   def get_available_docks
     #return number of empty docks at station
-    @num_docks - @bikes.count
+    num_docks - docked_bikes.count
   end
 end
