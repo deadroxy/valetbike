8c0bb411ab2005e82359586dee206fad6748388f
yina <ybao81@smith.edu>
2023-03-28 17:55:15 -0400

    login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   
 #  get "/stations/:identification", to: "stations#index"
     get "/stations/:identifier", to: "station#reverse", as: "station_rev"
+
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
+
 end
