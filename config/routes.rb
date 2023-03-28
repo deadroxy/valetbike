5991117d0e6683e627e0b5e1bbaad34e9f80cf1c
glory <gloryugwu04@gmail.com>
2023-03-28 13:44:30 -0400

    added routes for how-it-works, payment, and locations pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: "stations#index"
+
+  match "how-it-works", to:"how_it_works#index", via: :get
+
+  match "payments", to:"payments#index", via: :get
+
+  match "locations", to:"stations#index", via: :get
 end
