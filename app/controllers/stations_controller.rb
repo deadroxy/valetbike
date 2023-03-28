ebefa7843afdc836c84d3dc357fb127e57bf93f5
PurplePanda95 <epneuwalder@gmail.com>
2023-03-27 21:55:39 -0400

    added CRUD to station_controller, views and routes

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,5 +4,45 @@ class StationsController < ApplicationController
     @stations = Station.all.order(identifier: :asc)
     @bikes = Bike.all.order(identifier: :asc)
   end
+  def new
+    @station = Station.new
+  end
+
+  def create
+    @station = Station.new(station_params)
+    
+    if @station.save
+      redirect_to(stations_path)
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @station = station.find(params[:id])
+  end
+
+  def update
+    @station = station.find(params[:id])
+    if @station.update(station_params)
+      redirect_to(stations_path)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @station = station.find(params[:id])
+  end
+
+  def destroy
+    @station = station.find(params[:id])
+    @station.destroy
+    redirect_to(stations_path)
+  end
+  private
   
+  def station_params
+    params.require(:station).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get 'membership/edit'
   get 'membership/delete'
 
+  get 'station/index'
+  get 'station/show'
+  get 'station/new'
+  get 'station/edit'
+  get 'station/delete'
+
   get 'users/index'
   get 'users/new'
   get 'users/edit'
