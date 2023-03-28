5ee31ef4398cf7aa2a1edc6803e0906ded96495d
ariaramanathan <aramanathan@smith.edu>
2023-03-28 06:59:55 -0400

    Added css, fixed login in, editted sing up/login links and landing pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     def create
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
-          sessions[:user_id] = user.id 
+          session[:user_id] = user.id 
           redirect_to '/welcome'
       end
     end
