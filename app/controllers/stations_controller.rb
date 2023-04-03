993aeaecf459c7f54ff2c12e8f3554d7c2b12c16
gugwu <gugwu@smith.edu>
2023-04-03 11:48:55 -0400

    Enable more functionality on viewing locations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,12 +3,33 @@ class StationsController < ApplicationController
   def index
     # @stations = Station.all.order(identifier: :asc)
     @stations = [{identifier: "wuieiu", name: "Atkins Farms Country Market", address: "Amherst, Massachusetts"}, {identifier: "edyhjds", name: "Atkins Farms Country Market", address: "South Amherst, Massachusetts"}, {identifier: "ewieio", name: "Atkins Farms Country Market", address: "Northampton, Massachusetts"}]
+    @stations = Station.search(params[:search])
+
+
     @stations.each do |station| 
       puts station[:identifier]
       puts station[:name]
       puts station[:address]
     end
 
+  end
+
+  def create 
+    @station = Station.new(station_params)
+    
+  end
+
+
+  private
+
+  def station_params
+    params.require(:station).permit(
+      :name, 
+      :street,
+      :city,
+      :state,
+      :country,
+      :zipcode,)
   end
 
 end
