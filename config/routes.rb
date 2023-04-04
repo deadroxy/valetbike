e2f14a221206230e5f2c26302115f51cd55ec86f
Ashleyjagai <ajagai@smith.edu>
2023-04-03 22:54:46 -0400

    set up main controller and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  root to: "stations#index"
+  resources :users, only: [:new, :create]
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   get 'welcome', to: 'sessions#welcome'
+   get 'authorized', to: 'sessions#page_requires_login'
+
+  resources :users
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
+  get 'main/index'
+  get 'main/tutorial'
+  get 'main/help'
+  root to: "main#index"
+  #root to: "stations#index"
 end
