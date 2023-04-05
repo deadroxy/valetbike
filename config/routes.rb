55e59eb034f00e4fc6575bfdca0b249e76f32bfe
SophiaDai1206 <sdai33@smith.edu>
2023-04-05 12:26:05 -0400

    with error

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-   get 'login', to: 'sessions#new'
-   post 'login', to: 'sessions#create'
-   get 'welcome', to: 'sessions#welcome'
-   get 'authorized', to: 'sessions#page_requires_login'
-
-  resources :users
-  
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  root to: "main#index"
   get 'main/index'
   get 'main/tutorial'
   get 'main/help'
-  root to: "main#index"
+
+  resources :users, only: [:new, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'main/welcome', to: 'sessions#welcome'
+  
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  
+
+  resources :users
+  
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
+  # get 'users/new'
+  # get 'users/create'
+
   #root to: "stations#index"
 end
