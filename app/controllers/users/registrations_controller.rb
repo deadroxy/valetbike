1d427938894f8018d4fec7cb8327b64d7f691d68
PurplePanda95 <epneuwalder@gmail.com>
2023-04-09 18:07:18 -0400

    debugged controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user! 
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :authenticate_user! 
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -54,8 +54,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-     path = super(resource) 
-     path = cards_new_path if path == root_path
+     path = super(resource)
+     path = pages_about_us_path if path == root_path 
+     #path = cards_new_path if path == root_path
      #path   
    end
 end
