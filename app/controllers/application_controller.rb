5efb14fe8955237dc324e0e87249594a5444cffe
Your Name <me@example.com>
2023-04-09 17:34:43 -0400

    change password and login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
 
+    before_action :set_current_user
+
 
     helper_method :current_user, :logged_in?
+    
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find_by(id: session[:user_id])
+        end
+    end
+
+    def require_user_log_in!
+        redirect_to login_path, alert: "You must be signed in to do that." if Current.user.nil?
+    end
+
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
