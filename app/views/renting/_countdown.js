71ecdd697921b7d058259993fbb600931208cd46
yonglinh <huang58y@mtholyoke.edu>
2023-04-10 12:14:24 -0400

    countdown final

--- app/views/renting/_countdown.js
+++ app/views/renting/_countdown.js
@@ -4,8 +4,6 @@ import "jquery";
 $( document ).ready(function() {
 
 
-
-    //var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
     // Set the date we're counting down to
     var countDownDate = new Date($('.temp_information').data('temp')).getTime();
 
@@ -19,18 +17,20 @@ $( document ).ready(function() {
     var distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((distance % (1000 * 60)) / 1000);
+
+    var hDisplay = h > 0 ? (h < 10 ? "0" + h + ":" : h + ":") : "00:";
+    var mDisplay = m > 0 ? (m < 10 ? "0" + m + ":" : m + ":") : "00:";
+    var sDisplay = s > 0 ? (s < 10 ? "0" + s : s) : "00";
 
     // Display the result in the element with id="demo"
-    document.getElementById("clock").innerHTML = hours + "h "
-    + minutes + "m " + seconds + "s ";
-
+    document.getElementById("clock").innerHTML =  hDisplay+mDisplay+sDisplay;
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("clock").innerHTML = "EXPIRED";
+        document.getElementById("clock").innerHTML = "<style= 'background-color:powderblue' > EXPIRED </style>" ;
     }
     }, 1000);
 
