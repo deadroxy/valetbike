4a4608ff14c00391324e444551fe5ba7ac109f39
AfeyaJahin <jahin22a@mtholyoke.edu>
2023-04-16 16:43:53 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_170637) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_021515) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_170637) do
 
   create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "cardholder_name"
-    t.integer "card_number"
+    t.bigint "card_number"
     t.date "expiration_date"
     t.integer "cvv"
     t.float "card_balance"
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_170637) do
 
   create_table "rentings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "bike_id"
-    t.string "current_station_name"
-    t.integer "current_station_id"
+    t.integer "start_station_id"
+    t.integer "end_station_id"
     t.datetime "startTime"
     t.datetime "endTime"
     t.boolean "status"
@@ -63,19 +63,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_170637) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
-  end
-
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.string "name"
-    t.string "address"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -90,7 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_170637) do
     t.datetime "updated_at", null: false
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
-    t.string "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
