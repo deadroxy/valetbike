25061e26d7842b9cad5f057ca5a4007ec5d45858
Priya Dalal-Whelan <priyadw00@gmail.com>
2023-04-17 20:42:02 -0400

    last yesterday edits

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,8 +6,17 @@ class RentalsController < ApplicationController
     end
     
     def create
-        @rental = Rental.create(params.require(:user_id).permit(:user_id, :bike_id, :start_time, :start_station_id,))
-        redirect_to '/welcome'
+        @rental = Rental.new(params[:f])
+        if @rental.save 
+            redirect_to '/profile' # this should actually go to the active ride page, just wanted somwhere random to send it for now 
+        else
+            render('new') 
+            puts "save failed"
+        end 
+
+
+       #@rental = Rental.create(params.require(:user_id).permit(:user_id, :bike_id, :start_time, :start_station_id,))
+        #redirect_to '/welcome'
     #redirect_to '/welcome'
         #instantate using
         #current user
