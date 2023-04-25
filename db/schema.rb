9ba46f3d71e290c7ee254218864d7f408029eee3
cyberchai <chairachananharder@gmail.com>
2023-04-25 17:50:57 -0400

    mapbox implemented on new home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_163516) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_223656) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -34,13 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_163516) do
     t.integer "identifier"
     t.string "name"
     t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_logins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
