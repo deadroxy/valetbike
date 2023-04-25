aa1b3d60d33886f4f92b3354da658da47b10655d
PurplePanda95 <epneuwalder@gmail.com>
2023-04-25 14:05:18 -0400

    Add default position to membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,11 @@ class Membership < ApplicationRecord
     validates_presence_of   :identifier,
                             :name,
                             :position
+    before_validation :set_default_position,
+        if: Proc.new {|t| t.position.blank? || t.position < 0 }
+    private
+    def set_default_position
+        max = Membership.maximum(:position) || 0
+        self.position = max + 1
+    end
 end
