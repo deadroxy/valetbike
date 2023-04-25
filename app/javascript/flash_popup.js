cccde93072f164ca0fcf07be4bf6675df80245b0
AfeyaJahin <jahin22a@mtholyoke.edu>
2023-04-25 15:15:17 -0400

    add flash message

---
+++ app/javascript/flash_popup.js
@@ -0,0 +1,7 @@
+// app/javascript/flash_popup.js
+
+function closeFlashPopup() {
+    var flashPopup = document.getElementById("flash-popup");
+    flashPopup.style.display = "none";
+  }
+  
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,4 +10,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails-ujs", to: "rails-ujs@6.1.4/lib/assets/compiled/rails-ujs.js"
 # ... other entries ...
-
+pin "flash_popup", to: "flash_popup.js", preload: true
