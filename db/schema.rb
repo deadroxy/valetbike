298e9f03d2dd55665edc492c7943b60fe5e32003
@HualongXu <hxu@smith.edu>
2023-04-26 01:17:59 -0400

    Some Membership FrontEnd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_183426) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_045830) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_183426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
+    t.string "category"
   end
 
   create_table "payments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
