1f3d731d7b2008637ad0b5aefb4326594cd41875
SophiaDai1206 <sdai33@smith.edu>
2023-04-28 15:00:35 -0400

    scooters' numbers match

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,20 +2,24 @@ class Station < ApplicationRecord
   validates_presence_of    :identifier,
                            :name,
                            :address
+                           :docked_bike_count
   validates_uniqueness_of  :identifier
 
   has_many :bikes
   
   #has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
+  has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id, primary_key: :identifier
 
   def to_param
     identifier
   end
 
   def num_docks_available
-    dock_count - docked_bike_count
+    dock_count - docked_bike_count 
   end 
 
-  
+  def num_bikes_available
+    docked_bike_count 
+  end 
   
 end
