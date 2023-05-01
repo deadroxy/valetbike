0088165d54c5b73f962be6c10400ed8b1e680f17
Yina Bao <124383706+ybao81@users.noreply.github.com>
2023-04-30 21:55:31 -0400

    fix sign up error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
-    validates_uniqueness_of  :email,
-                             :phone_num
-                             
+    #validates_uniqueness_of  :email
 end
