d3b37c42895c823b660d1ade88811adff6dfffc6
Your Name <me@example.com>
2023-05-01 19:29:19 -0400

    created error messages for login, logoout, and new user. redundant email ip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    
-    validates_uniqueness_of :email
-
+    validates_uniqueness_of :email, presence: true
     validates_uniqueness_of :phone_num
+    validates :username, presence: true
 end
