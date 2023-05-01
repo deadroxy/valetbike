c814258d3a2677673e352ebb632fb822b2c0e933
Yina Bao <124383706+ybao81@users.noreply.github.com>
2023-04-30 22:21:14 -0400

    change syntax error to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    #validates_uniqueness_of  :email
+    
+    validates_uniqueness_of :email
+
+    validates_uniqueness_of :phone_num
 end
