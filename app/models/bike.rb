76b089b56d00d85a571d7418ecc533d9cbbd8abd
Hamssatou A. Maiga <95487735+HamssatouM@users.noreply.github.com>
2023-05-01 23:47:22 -0400

    Update bike.rb
    invalide current_statiob_id this was causing the bike to not unlock

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,11 +1,11 @@
 class Bike < ApplicationRecord
-
-  validates_presence_of    :identifier,
-                           :current_station_id
-                           #:station_id
+  :current_station_identifier
+  validates_presence_of    :identifier 
  
+                         #  :station_id
  
   validates_uniqueness_of  :identifier
+
 
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
 
