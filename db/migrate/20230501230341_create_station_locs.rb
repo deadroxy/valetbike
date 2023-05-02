94ad0d650511af508bebf066612a20c8613460e2
Hamssatou A. Maiga <95487735+HamssatouM@users.noreply.github.com>
2023-05-01 23:58:26 -0400

    delete drop table

--- db/migrate/20230501230341_create_station_locs.rb
+++ db/migrate/20230501230341_create_station_locs.rb
@@ -1,6 +1,5 @@
 class CreateStationLocs < ActiveRecord::Migration[7.0]
   def change
-    drop_table :station_locs
     create_table :station_locs do |t|
       t.string :name
       t.string :address
