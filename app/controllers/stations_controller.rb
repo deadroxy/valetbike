29d044db091da9ae6a05f4a960c18cee569a6453
gugwu <gugwu@smith.edu>
2023-05-05 19:57:59 -0400

    Assignment 1

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,6 +2,11 @@ class StationsController < ApplicationController
   
   def index
     @stations = Station.all.order(identifier: :asc)
+    @bikes = Bike.all.order(identifier: :asc)
+    if params[:view]
+      @view = params[:view]      
+    else
+      @view = "stations"
+    end
   end
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_090719) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_203350) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bikes_rentings", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "renting_id", null: false
+    t.bigint "bike_id", null: false
+    t.index ["bike_id", "renting_id"], name: "index_bikes_rentings_on_bike_id_and_renting_id"
+    t.index ["renting_id", "bike_id"], name: "index_bikes_rentings_on_renting_id_and_bike_id"
+  end
+
+  create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "cardholder_name"
+    t.integer "user_id"
+    t.decimal "card_number", precision: 10
+    t.date "expiration_date"
+    t.integer "cvv"
+    t.float "card_balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rentings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "bike_id"
+    t.integer "start_station_id"
+    t.integer "end_station_id"
+    t.datetime "startTime"
+    t.datetime "endTime"
+    t.boolean "status"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rentings_stations", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "renting_id", null: false
+    t.bigint "station_id", null: false
+    t.index ["renting_id", "station_id"], name: "index_rentings_stations_on_renting_id_and_station_id"
+    t.index ["station_id", "renting_id"], name: "index_rentings_stations_on_station_id_and_renting_id"
+  end
+
+  create_table "services", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "bike_number"
+    t.string "bike_station"
+    t.string "additional_information"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -24,6 +71,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_090719) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "provider", limit: 50, default: "", null: false
+    t.string "uid", limit: 500, default: "", null: false
+    t.string "fullname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
